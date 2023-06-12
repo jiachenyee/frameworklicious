b32b0251a3c94587874a8472a1243fdb38d1363c
Jia Chen Yee <github@jiachen.app>
2023-06-12 09:08:41 +0700

    add individual projects

---
+++ Machine Learning/Demo Project/Demo_ProjectApp.swift
@@ -0,0 +1,20 @@
+//
+//  Demo_ProjectApp.swift
+//  Demo Project
+//
+//  Created by Jia Chen Yee on 12/06/23.
+//
+
+import SwiftUI
+import FrameworkliciousCore
+
+@main
+struct Demo_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FrameworkliciousApp(framework: .machineLearning) {
+                ContentView()
+            }
+        }
+    }
+}
