85aef8682f85691b4ebe3a315685a56c0371f8b5
Dian DInihari | Tia <55896324+ddhyuliani@users.noreply.github.com>
2023-06-27 16:56:33 +0700

    Healthkit demo wrapped up

---
+++ HealthKit/Demo Project/Managers/HealthStore.swift
@@ -0,0 +1,59 @@
+//
+//  HealthStore.swift
+//  Demo Project
+//
+//  Created by Dini on 27/06/23.
+//
+
+import Foundation
+import SwiftUI
+import HealthKit
+
+class HealthStore {
+    
+    var healthStore: HKHealthStore?
+    var query: HKStatisticsCollectionQuery?
+    
+    init() {
+        if HKHealthStore.isHealthDataAvailable() {
+            healthStore = HKHealthStore()
+        }
+    }
+    
+    func calculateSteps(completion: @escaping (HKStatisticsCollection?) -> Void) {
+        
+        let stepType = HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)!
+        
+        let startDate = Calendar.current.date(byAdding: .day, value: -6, to: Date())
+        
+        let anchorDate = Date.mondayAt12AM()
+        
+        let daily = DateComponents(day: 1)
+        
+        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: Date(), options: .strictStartDate)
+        
+        query = HKStatisticsCollectionQuery(quantityType: stepType, quantitySamplePredicate: predicate, options: .cumulativeSum, anchorDate: anchorDate, intervalComponents: daily)
+        
+        query!.initialResultsHandler = { query, statisticsCollection, error in
+            completion(statisticsCollection)
+        }
+        
+        if let healthStore = healthStore, let query = self.query {
+            healthStore.execute(query)
+        }
+        
+    }
+    
+    func requestAuthorization(completion: @escaping (Bool) -> Void) {
+        
+        let stepType = HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)!
+        
+        guard let healthStore = self.healthStore else { return completion(false) }
+        
+        healthStore.requestAuthorization(toShare: [], read: [stepType]) { (success, error) in
+            completion(success)
+        }
+        
+    }
+    
+}
